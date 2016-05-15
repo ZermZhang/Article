e5c59752a96eade64875787c5884b2660c5a277c
zz-kelekele <zhang371312@126.com>
2016-05-15 13:58:37 +0800

    modify the precision for the error

--- MsFEM_code/main_MsFEM_2d.m
+++ MsFEM_code/main_MsFEM_2d.m
@@ -19,8 +19,8 @@ solve_fine = 1;
 compute_error = 1;
 
 % Input the num of Corase block , num of fine block in one Corase block 
-Nx=16; Ny=Nx;       %num of corase block 
-nx=16; ny=nx;       %num of fine block in one corase block
+Nx=32; Ny=Nx;       %num of corase block 
+nx=32; ny=nx;       %num of fine block in one corase block
 
 
 %=========================================================
@@ -34,10 +34,10 @@ epsilon = 1/100;
 
 %source_a = @(x,y) (4 + sin(pi*(sqrt(y/epsilon)-x/epsilon)))./(2 + sin(pi*(y/epsilon-2*x/epsilon)));
 % source_a = @(x,y) (5 + sin(pi*(sqrt(y/epsilon)-x/epsilon)))./(2 + sin(pi*(y-x.^2).*(y/epsilon-2*x/epsilon)));
-%source_a = @(x,y) (2 + 1.8*sin(2*pi*x/epsilon))./(2+1.8*cos(2*pi*y/epsilon))...
-%    +(2+sin(2*pi*y/epsilon))./(2+1.8*sin(2*pi*x/epsilon));
+source_a = @(x,y) (2 + 1.8*sin(2*pi*x/epsilon))./(2+1.8*cos(2*pi*y/epsilon))...
+    +(2+1.8*sin(2*pi*y/epsilon))./(2+1.8*sin(2*pi*x/epsilon));
 
-source_a = @(x,y) (1+0*x);
+%source_a = @(x,y) (1+0*x);
 
 a = source_a(X1,Y1);
 
@@ -125,7 +125,7 @@ if compute_error
 DG_error = sqrt(((MS_U(:)-Global_U(:))'*Global_DA*(MS_U(:)-Global_U(:)))/((Global_U(:))'*Global_DA*(Global_U(:))));
 L2_error = sqrt(((MS_U(:)-Global_U(:))'*Global_M*(MS_U(:)-Global_U(:)))/((Global_U(:))'*Global_M*(Global_U(:))));
 
-fprintf('The relative Energy Error for MsFEM is  %2.2f%%. \n',DG_error*100)
-fprintf('The relative L2 Error for MsFEM is      %2.2f%%. \n',L2_error*100)
+fprintf('The relative Energy Error for MsFEM is  %2.8f. \n',DG_error)
+fprintf('The relative L2 Error for MsFEM is      %2.8f. \n',L2_error)
 end
 %=========================================================
